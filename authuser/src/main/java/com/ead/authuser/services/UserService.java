d937e18b80240d668ed0785042ebb30f045f13e2
Jonathan Philipe <sicrano3000@gmail.com>
2022-09-08 14:15:30 -0300

    Adicionando paginação

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ead.authuser.models.UserModel;
 
 public interface UserService {
 
 	List<UserModel> findAll();
+	
+	Page<UserModel> findAll(Pageable pageable);
 
 	Optional<UserModel> findById(UUID userId);
 
--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ead.authuser.models.UserModel;
@@ -20,6 +22,11 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<UserModel> findAll() {
 		return userRepository.findAll();
+	}
+
+	@Override
+	public Page<UserModel> findAll(Pageable pageable) {
+		return userRepository.findAll(pageable);
 	}
 
 	@Override
