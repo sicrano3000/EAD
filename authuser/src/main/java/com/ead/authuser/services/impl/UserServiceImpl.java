69f8a8c919be483c8ea2b19795b9bf47c26a385b
Jonathan Philipe <sicrano3000@gmail.com>
2022-09-08 14:44:27 -0300

    Adicionando specification na paginação

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.ead.authuser.models.UserModel;
@@ -25,8 +26,8 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public Page<UserModel> findAll(Pageable pageable) {
-		return userRepository.findAll(pageable);
+	public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+		return userRepository.findAll(spec, pageable);
 	}
 
 	@Override
