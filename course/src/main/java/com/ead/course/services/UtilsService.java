086b295d6e5e7a965680d6f4c295c86661a7baf2
Jonathan Philipe <sicrano3000@gmail.com>
2022-11-07 17:41:49 -0300

    API Composition Pattern em Course Microservice

---
+++ course/src/main/java/com/ead/course/services/UtilsService.java
@@ -0,0 +1,11 @@
+package com.ead.course.services;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+
+public interface UtilsService {
+	
+	String createUrl(UUID userId, Pageable pageable);
+
+}
