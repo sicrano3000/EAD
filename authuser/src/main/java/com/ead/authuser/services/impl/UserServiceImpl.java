006564705bf37f165494b8c5241758c6645b8c30
Jonathan Philipe <sicrano3000@gmail.com>
2022-11-26 15:33:44 -0300

    Preparação de Deleção Entre Microservices

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,24 +1,28 @@
 package com.ead.authuser.services.impl;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.ead.authuser.models.UserModel;
-import com.ead.authuser.repositories.UserRepository;
-import com.ead.authuser.services.UserService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+
+    @Autowired
+    private UserCourseRepository userCourseRepository;
 
 	@Override
 	public List<UserModel> findAll() {
@@ -35,8 +39,14 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findById(userId);
 	}
 
+    @Transactional
 	@Override
 	public void delete(UserModel userModel) {
+        var userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
+        if (!userCourseModelList.isEmpty()) {
+            userCourseRepository.deleteAll(userCourseModelList);
+        }
+
 		userRepository.delete(userModel);
 	}
 
--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,24 +1,23 @@
 package com.ead.course.services.impl;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import javax.transaction.Transactional;
-
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.CourseUserRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import com.ead.course.models.CourseModel;
-import com.ead.course.models.LessonModel;
-import com.ead.course.models.ModuleModel;
-import com.ead.course.repositories.CourseRepository;
-import com.ead.course.repositories.LessonRepository;
-import com.ead.course.repositories.ModuleRepository;
-import com.ead.course.services.CourseService;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -31,6 +30,9 @@ public class CourseServiceImpl implements CourseService {
 	
 	@Autowired
 	private LessonRepository lessonRepository;
+
+    @Autowired
+    private CourseUserRepository courseUserRepository;
 
 	@Transactional
 	@Override
@@ -48,6 +50,11 @@ public class CourseServiceImpl implements CourseService {
 			
 			moduleRepository.deleteAll(moduleModelList);
 		}
+
+        var courseUserModelList = courseUserRepository.findAllCourseUserIntoCourse(courseModel.getCourseId());
+        if (!courseUserModelList.isEmpty()) {
+            courseUserRepository.deleteAll(courseUserModelList);
+        }
 		
 		courseRepository.delete(courseModel);
 	}
