dc8bfd2bb1a0c1c31b771f776a53cbf6b8a61132
Jonathan Philipe <sicrano3000@gmail.com>
2023-09-16 15:49:40 -0300

    Implementando Retry Resilience4j e Configurando Timeout

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,11 +3,13 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,7 +33,8 @@ public class CourseClient {
 
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
-	
+
+	@Retry(name = "retryInstance", fallbackMethod = "retryFallback")
 	public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
 		List<CourseDto> searchResult = null;
 		ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -53,6 +57,13 @@ public class CourseClient {
 		log.info("Ending request /courses userId {}", userId);
 		
 		return result.getBody();
+	}
+
+	public Page<CourseDto> retryFallback(UUID userId, Pageable pageable, Throwable t) {
+		log.error("Insede retry retryfallback, cause - {}", t.toString());
+		List<CourseDto> searchResult = new ArrayList<>();
+
+		return new PageImpl<>(searchResult);
 	}
 
 }
